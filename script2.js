4eb1ccd8cd3099393e940ee0607fba669ef18279
Jiyun <grosjeanjiyun@icloud.com>
2024-03-12 10:46:37 +0100

    ex2 done

---
+++ script2.js
@@ -0,0 +1,23 @@
+//  Puis ajouter un script (dans un autre fichier) permettant de trouver le mot le plus long d'une phrase. Le tout devant être affiché dans
+
+function findString(array) {
+  let longestString = "";
+
+  array.forEach(function (string) {
+    if (string.length > longestString.length) {
+      longestString = string;
+    }
+  });
+
+  return longestString;
+}
+
+let string = findString([
+  "afsjk",
+  "akhfjsdl",
+  "ldjh",
+  "c'est la plus longue phrase",
+  "hkjfsdllfjhsnv",
+]);
+
+console.log(string);
