6a18d6eb2a553621d2bbce9c9b459b9750d7dddf
Jiyun <grosjeanjiyun@icloud.com>
2024-03-12 10:30:00 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// run in terminal : node script.js
+
+// Coder en JS un script intégré à un fichier html permettant de retrouver le plus grand
+// nombre d un tableau (tableau en dur). Puis ajouter un script (dans un autre fichier) permettant de trouver le mot le plus long d'une phrase. Le tout devant être affiché dans
+
+console.log("ici");
