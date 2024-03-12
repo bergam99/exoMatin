1eb17ed2be2c442ddff4949c58e5525ee26f5694
Jiyun <grosjeanjiyun@icloud.com>
2024-03-12 10:38:53 +0100

    ex1 done

--- script.js
+++ script.js
@@ -4,3 +4,7 @@
 // nombre d un tableau (tableau en dur). Puis ajouter un script (dans un autre fichier) permettant de trouver le mot le plus long d'une phrase. Le tout devant être affiché dans
 
 console.log("ici");
+
+// retrouver le plus grand nombre d un tableau
+let tab1 = [1, 3, 5, 6, 8, 9, 142, 153, 4268, 1342, 4, 6234, 3];
+console.log(Math.max(...tab1));
